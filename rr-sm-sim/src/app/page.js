85e6ca0ee62f49c80f06a5498367112da593c259
Ober11 <126175091+Ober11@users.noreply.github.com>
2024-06-06 22:05:07 +0200

    tried out border options

--- rr-sm-sim/src/app/page.js
+++ rr-sm-sim/src/app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-row bg-center justify-center flex-nowrap" >
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-row bg-center justify-center flex-nowrap slots-border">
         <p>Test</p>
         <p>Test</p>
         <p>Test</p>
