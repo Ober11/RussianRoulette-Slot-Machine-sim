f3a006762fda62cf1abe5ed1263049bf0a61134f
Ober11 <126175091+Ober11@users.noreply.github.com>
2024-06-06 21:11:56 +0200

    small fix

--- rr-sm-sim/src/app/page.js
+++ rr-sm-sim/src/app/page.js
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-row bg-center justify-center flex-nowrap" >
-        <p>Skibidi</p>
-        <p>Skibidi</p>
-        <p>Skibidi</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
       </div>
     </main>
   );
